7b3f01d01fe81a7fbfa1e0ffe08131a1adce1fc4
Teddy Lelong <teddy@webalgo.fr>
2022-08-22 11:04:30 +0200

    feat: added functional test to homepage

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,13 +6,50 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    private $client = null;
+
+    public function setUp()
+    {
+        $this->client = static::createClient(
+            array(), array(
+                'PHP_AUTH_USER' => 'teddy',
+                'PHP_AUTH_PW'   => 'teddy',
+            )
+        );
+    }
+
+    /**
+     * Test homepage with already logged in user
+     *
+     * @return void
+     */
+    public function testIndexUserIsLoggedIn()
+    {
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $this->assertContains(
+            "Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !",
+            $crawler->filter('h1')->text()
+        );
+
+        // var_dump($this->client->getResponse()->getContent());
+    }
+
+    /**
+     * Test homepage without user logged in
+     *
+     * @return void
+     */
+    public function testIndexUserIsNotLoggedIn()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertTrue(
+            $client->getResponse()->isRedirect('http://localhost/login'),
+            'response is a redirect to /login'
+        );
     }
 }
