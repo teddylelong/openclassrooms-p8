83dd3c51d51a87d62ad68a3026af5310772bc2d3
Teddy Lelong <teddy@webalgo.fr>
2022-08-23 15:04:50 +0200

    fix: removed some minor depreciations

--- todo_and_co/src/AppBundle/Controller/SecurityController.php
+++ todo_and_co/src/AppBundle/Controller/SecurityController.php
@@ -5,16 +5,15 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
     /**
      * @Route("/login", name="login")
      */
-    public function loginAction(Request $request)
+    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
     {
-        $authenticationUtils = $this->get('security.authentication_utils');
-
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
--- todo_and_co/web/app_dev.php
+++ todo_and_co/web/app_dev.php
@@ -24,7 +24,6 @@ $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 Request::setTrustedProxies(['~'], Request::HEADER_X_FORWARDED_ALL);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
