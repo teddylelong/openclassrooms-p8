75361248f1f77dafebe167eda86d7ff458758c47
Teddy Lelong <teddy@webalgo.fr>
2022-08-24 07:34:48 +0200

    fix: migrated to SF 4.4

--- todo_and_co/src/AppBundle/Controller/SecurityController.php
+++ todo_and_co/src/AppBundle/Controller/SecurityController.php
@@ -4,7 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
--- todo_and_co/src/AppBundle/Controller/UserController.php
+++ todo_and_co/src/AppBundle/Controller/UserController.php
@@ -4,9 +4,9 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends Controller
 {
