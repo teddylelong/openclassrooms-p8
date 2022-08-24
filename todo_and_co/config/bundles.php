c2d6296c08a93cbf1f26db885d5f4d678fc0065d
Teddy Lelong <teddy@webalgo.fr>
2022-08-24 09:57:08 +0200

    fix: migrate Doctrine configuration files

--- todo_and_co/config/bundles.php
+++ todo_and_co/config/bundles.php
@@ -14,5 +14,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
