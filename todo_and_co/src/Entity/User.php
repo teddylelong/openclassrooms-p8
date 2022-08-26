359ca89dfe3036a0159444398daf4e8a0cd803bb
Teddy Lelong <teddy@webalgo.fr>
2022-08-26 15:00:36 +0200

    fix: solved some minors SymfonyInsigth

--- todo_and_co/src/Entity/User.php
+++ todo_and_co/src/Entity/User.php
@@ -63,7 +63,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
@@ -107,8 +106,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function eraseCredentials()
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
     }
 
     public function getEmail(): ?string
